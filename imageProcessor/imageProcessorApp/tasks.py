723caf6fc3c3c4a75215b243795789d4b6488868
Claude Ebaneck <claudeforlife@gmail.com>
2021-03-29 22:17:29 +0200

    ensure `accepted||rejected` image formats are consistent

--- imageProcessor/imageProcessorApp/tasks.py
+++ imageProcessor/imageProcessorApp/tasks.py
@@ -20,12 +20,14 @@ def add_waterMark(photo_id, selected_flag):
 
     if photo:
         image = Image.open(photo.photo.file)
+        # wrap above in try/except block
+        image_format = image.format.lower()
         watermark_img_name = settings.STATIC_ROOT + "/rejected" + '.jpeg'
         if(selected_flag == "accepted"):
             watermark_img_name = settings.STATIC_ROOT + "/accepted" + '.jpeg'
         watermark = Image.open(watermark_img_name)
         image.paste(watermark, (0, 0), watermark.convert('RGBA'))
-        image.save(str(settings.CUSTOM_PATH) + photo.photo.url, format="png")
+        image.save(str(settings.CUSTOM_PATH) + photo.photo.url, format=image_format)
         return "Task completed!!"
 
     return None
