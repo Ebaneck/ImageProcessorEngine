12a88d23974cd0ce6ee71d8d7f25e1e132c6a7de
Claude Ebaneck <claudeforlife@gmail.com>
2021-03-29 10:16:48 +0200

    add and initialize Django project files

---
+++ imageProcessor/imageProcessor/celery.py
@@ -0,0 +1,13 @@
+import os
+
+from celery import Celery
+
+from django.conf import settings
+
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'imageProcessor.settings')
+
+app = Celery('imageProcessor')
+app.config_from_object('django.conf:settings', namespace='CELERY')
+app.autodiscover_tasks()
+# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
